1b536985f1102b833a87ff9dd9dfbcbc3c36be46
thesmartwon <thesmartwon@live.com>
2019-06-09 10:32:50 -0400

    bugfix

--- renderer.js
+++ renderer.js
@@ -1,12 +1,16 @@
 const assert = require('assert')
 const { tagTypes } = require('./constants')
 
+// All verses must have a parent
+const NO_OPRHAN_VERSES = true
+
 const renderTokens = tokens => {
 	const res = { chapters: [] }
 	let curChapter = { children: [] }
 
 	tokens.forEach(token => {
 		const curChild = curChapter.children[curChapter.children.length - 1]
+
 		if (tagTypes.identification.includes(token.tag)) {
 			res[token.tag] = token.text
 		}
@@ -19,11 +23,11 @@ const renderTokens = tokens => {
 			curChapter.intro.push(token)
 		}
 		else if (token.tag === 'c') {
-			res.chapters.push({num: token.num, ...curChapter})
-			if (res.chapters.length > 1) curChapter = { intro: [], children: [] }
-		}
-		else if (res.chapters.length === 0) {
-			return
+			curChapter.num = token.num
+			if (res.chapters.length > 0) {
+				res.chapters.push(curChapter)
+				curChapter = { children: [] }
+			}
 		}
 		else if (tagTypes.paragraph.includes(token.tag)) {
 			curChapter.children.push({ children: [], ...token })
@@ -39,12 +43,15 @@ const renderTokens = tokens => {
 			if (curChild && tagTypes.paragraph.includes(curChild.tag)) {
 				curChild.children.push(token)
 			}
+			else if (NO_OPRHAN_VERSES && token.tag === 'v') {
+				curChapter.children.push({ tag: 'p', children: [token], auto: true })
+			}
 			else {
 				curChapter.children.push(token)
 			}
 		}
 	})	
-	res.chapters.push(curChapter.intro.concat(curChapter.children))
+	res.chapters.push(curChapter)
 
 	return res
 }
