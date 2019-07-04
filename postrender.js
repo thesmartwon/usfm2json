7c1c34228f3320e208e50d8b6c475cac692d1c55
clicking buttons <clickingbuttons@pm.me>
2019-07-04 11:53:42 -0400

    fix postrender punctuation

--- postrender.js
+++ postrender.js
@@ -2,28 +2,34 @@ const assert = require('assert')
 const { tagTypes } = require('./constants')
 
 const postRender = json => {
-	// Move concurrent words/orphans into last verse, or into new paragraph
 	json.chapters.forEach(chapter => {
-		chapter.children.filter(child => child.children).forEach(child => {
-			const newChildren = []
-			child.children.forEach(child2 => {
-				const lastChild = newChildren[newChildren.length - 1]
-				if (lastChild && ['w', 'orphan'].includes(child2.tag)) {
-					if (!lastChild.text) {
-						lastChild.text = child2.text
-					}
-					else if (child2.tag === 'w') {
-						lastChild.text += ' ' + child2.text
+		chapter.children.forEach(child => {
+			// Remove trailing space after quote start
+			if (child.text) {
+				child.text = child.text.replace(/([“‘])\s+/, "$1")
+			}
+
+			// Move concurrent words/orphans into last verse, or into new paragraph
+			if (child.children) {
+				const newChildren = []
+				child.children.forEach(child2 => {
+					const lastChild = newChildren[newChildren.length - 1]
+					if (lastChild && ['w', 'orphan'].includes(child2.tag)) {
+						if (!lastChild.text) {
+							lastChild.text = child2.text
+						}
+						else {
+							if (/[\w”?.,!—]$/.test(lastChild.text) && /^[\w“—]/.test(child2.text))
+								lastChild.text += ' '
+							lastChild.text += child2.text
+						}
 					}
 					else {
-						lastChild.text += child2.text
+						newChildren.push(child2)
 					}
-				}
-				else {
-					newChildren.push(child2)
-				}
-			})
-			child.children = newChildren
+				})
+				child.children = newChildren
+			}
 		})
 	})
 
