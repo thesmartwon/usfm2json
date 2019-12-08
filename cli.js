0ee22e40ed2c2a6b51bfa7857123d681231f7efc
thesmartwon <thesmartwon@live.com>
2019-12-07 22:56:32 -0500

    v0.0.13

--- cli.js
+++ cli.js
@@ -4,5 +4,4 @@ const { usfm2json } = require('./lib/usfm2json')
 
 const book = fs.readFileSync(process.argv[2], 'utf8')
 const json = usfm2json(book)
-// console.log(JSON.stringify(json, null, 2))
-
+console.log(JSON.stringify(json, null, 2))
